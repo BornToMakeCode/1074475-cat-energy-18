66b4a3ee92ce6f962eedfd272a2a9be2e9044ba3
Sergey <s.yanacov@gmail.com>
2020-01-27 10:00:46 +0200

    Вторая версия страницы каталога

--- source/js/main-navigation.js
+++ source/js/main-navigation.js
@@ -4,12 +4,37 @@
   var menuButton = pageHeader.querySelector(".page-header__menu-toggle");
   var mainNavigation = pageHeader.querySelector(".main-navigation");
 
-  mainNavigation.classList.add("main-navigation--closed");
+  var setDefaultState = function(){
+    menuButton.classList.add("page-header__menu-toggle--closed");
+    mainNavigation.classList.add("main-navigation--closed");
+  }
+
+  var toggleMenu = function(){
+    if (mainNavigation.classList.contains('main-navigation--closed')) {
+      mainNavigation.classList.remove('main-navigation--closed');
+      menuButton.classList.remove('page-header__menu-toggle--closed');
+      mainNavigation.classList.add('main-navigation--opened');
+      menuButton.classList.add('page-header__menu-toggle--opened');
+    } else {
+      mainNavigation.classList.add('main-navigation--closed');
+      menuButton.classList.add('page-header__menu-toggle--closed');
+      mainNavigation.classList.remove('main-navigation--opened');
+      menuButton.classList.remove('page-header__menu-toggle--opened');
+    }
+  }
+
+
 
   menuButton.addEventListener("click", function (evt) {
     evt.preventDefault();
-    mainNavigation.classList.toggle("main-navigation--closed");
-});
+    toggleMenu();
+    // menuButton.classList.toggle("page-header__menu-toggle--closed");
+    // mainNavigation.classList.toggle("main-navigation--closed");
+  });
+
+
+  setDefaultState();
+
 
 })();
 
